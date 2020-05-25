1e78b873762361908c9a60ba84babbd08f6b772c
Shinjini Ghosh <shinghos@mit.edu>
2020-05-25 14:23:39 -0400

    minor reformat

--- shapes.js
+++ shapes.js
@@ -8,35 +8,36 @@ var $grid = $('.grid').isotope({
 // store filter for each group
 var filters = {};
 
-$('.filters').on( 'click', '.button', function( event ) {
-  var $button = $( event.currentTarget );
+$('.filters').on('click', '.button', function(event) {
+  var $button = $(event.currentTarget);
   // get group key
   var $buttonGroup = $button.parents('.button-group');
   var filterGroup = $buttonGroup.attr('data-filter-group');
   // set filter for group
-  filters[ filterGroup ] = $button.attr('data-filter');
+  filters[filterGroup] = $button.attr('data-filter');
   // combine filters
-  var filterValue = concatValues( filters );
+  var filterValue = concatValues(filters);
   // set filter for Isotope
-  $grid.isotope({ filter: filterValue });
+  $grid.isotope({
+    filter: filterValue
+  });
 });
 
 // change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function( event ) {
+$('.button-group').each(function(i, buttonGroup) {
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', 'button', function(event) {
     $buttonGroup.find('.is-checked').removeClass('is-checked');
-    var $button = $( event.currentTarget );
+    var $button = $(event.currentTarget);
     $button.addClass('is-checked');
   });
 });
-  
+
 // flatten object by concatting values
-function concatValues( obj ) {
+function concatValues(obj) {
   var value = '';
-  for ( var prop in obj ) {
-    value += obj[ prop ];
+  for (var prop in obj) {
+    value += obj[prop];
   }
   return value;
 }
-
