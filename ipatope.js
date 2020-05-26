d56a59c3d9d0f2951ef8cccd0d24b1b28ebb1c2c
Shinjini Ghosh <shinghos@mit.edu>
2020-05-26 00:42:04 -0400

    incorporating new data

--- ipatope.js
+++ ipatope.js
@@ -3,7 +3,7 @@
 
 // init Isotope
 var $grid = $('.grid').isotope({
-  itemSelector: '.phoneme-item', // TODO phoneme-item
+  itemSelector: '.phoneme-item',
   // layoutMode: 'fitRows',
 
   layoutMode: 'packery',
