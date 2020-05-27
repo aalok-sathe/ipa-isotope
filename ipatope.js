58df753b42238c6821e7e897e1e0702beed0a9c4
Shinjini Ghosh <shinghos@mit.edu>
2020-05-27 19:02:09 -0400

    corrected indentation

--- ipatope.js
+++ ipatope.js
@@ -3,98 +3,98 @@
 
 // init Isotope
 var $grid = $('.grid').isotope({
-    itemSelector: '.phoneme-item',
-    // layoutMode: 'fitRows',
+  itemSelector: '.phoneme-item',
+  // layoutMode: 'fitRows',
 
-    layoutMode: 'packery',
-    packery: {
-        gutter: 2
-    },
-    stamp: '.stamp',
+  layoutMode: 'packery',
+  packery: {
+    gutter: 2
+  },
+  stamp: '.stamp',
 
-    getSortData: {
-        name: '[name]',
-        symbol: '.symbol',
-        number: '.number parseInt',
-        sonority: '.sonority parseFloat',
-        category: '[data-category]',
-        weight: function(itemElem) {
-            var weight = $(itemElem).find('.weight').text();
-            return parseFloat(weight.replace(/[\(\)]/g, ''));
-        }
+  getSortData: {
+    name: '[name]',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    sonority: '.sonority parseFloat',
+    category: '[data-category]',
+    weight: function(itemElem) {
+      var weight = $(itemElem).find('.weight').text();
+      return parseFloat(weight.replace(/[\(\)]/g, ''));
     }
+  }
 });
 
 // filter functions
 var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-        var number = $(this).find('.number').text();
-        return parseInt(number, 10) > 50;
-    },
-    // show if sonority is greater than 3
-    sonGreaterThan3: function() {
-        var number = $(this).find('.sonority').text();
-        return parseFloat(number, 10) > 3;
-    },
-    // show if name ends with -ium
-    ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match(/ium$/);
-    }
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt(number, 10) > 50;
+  },
+  // show if sonority is greater than 3
+  sonGreaterThan3: function() {
+    var number = $(this).find('.sonority').text();
+    return parseFloat(number, 10) > 3;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match(/ium$/);
+  }
 };
 
 var filters = {}
 // bind filter button click
 $('.filters').on('click', '.button', function(event) {
-    // $checkbox = $(event.currentTarget);
-    // $button = $checkbox.parent('.button');
-    $button = $(event.currentTarget);
-    // get group key
-    var $buttonGroup = $button.parents('.button-group');
-    var filterGroup = $buttonGroup.attr('data-filter-group');
-    // set filter for group
-    var filterValue = filters[filterGroup] = $button.attr('data-filter');
-    // try to match function
-    filters[filterGroup] = filterFns[filterValue] || filters[filterGroup];
-    // combine filters
-    if (filterValue in filterFns) {
-        console.log(filterFns, filterValue);
-        $grid.isotope({
-            filter: filters[filterValue]
-        });
-    } else {
-        $grid.isotope({
-            filter: concatValues(filters)
-        });
-    }
-    // set filter for Isotope
+  // $checkbox = $(event.currentTarget);
+  // $button = $checkbox.parent('.button');
+  $button = $(event.currentTarget);
+  // get group key
+  var $buttonGroup = $button.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  var filterValue = filters[filterGroup] = $button.attr('data-filter');
+  // try to match function
+  filters[filterGroup] = filterFns[filterValue] || filters[filterGroup];
+  // combine filters
+  if (filterValue in filterFns) {
+    console.log(filterFns, filterValue);
+    $grid.isotope({
+      filter: filters[filterValue]
+    });
+  } else {
+    $grid.isotope({
+      filter: concatValues(filters)
+    });
+  }
+  // set filter for Isotope
 
 });
 
 // change is-checked class on buttons
 $('.button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', '.button', function(event) {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        var $button = $(event.currentTarget);
-        $button.addClass('is-checked');
-    });
+  var $buttonGroup = $(buttonGroup);
+  $buttonGroup.on('click', '.button', function(event) {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    var $button = $(event.currentTarget);
+    $button.addClass('is-checked');
+  });
 });
 
 // bind sort button click
 $('.sorters').on('click', '.button', function() {
-    var sortByValue = $(this).attr('data-sort-by');
-    $grid.isotope({
-        sortBy: sortByValue
-    });
+  var sortByValue = $(this).attr('data-sort-by');
+  $grid.isotope({
+    sortBy: sortByValue
+  });
 });
 
 
 $('.ui-group').packery({
-    itemSelector: '.ui-group-item',
-    gutter: 1,
-    percentPosition: true
+  itemSelector: '.ui-group-item',
+  gutter: 1,
+  percentPosition: true
 })
 
 // collection of Draggabillies
@@ -102,9 +102,9 @@ var draggies = [];
 var isDrag = false;
 
 // make all grid-items draggable
-$grid.find('.phoneme-item').each( function( i, gridItem ) {
-  var draggie = new Draggabilly( gridItem );
-  draggies.push( draggie );
+$grid.find('.phoneme-item').each(function(i, gridItem) {
+  var draggie = new Draggabilly(gridItem);
+  draggies.push(draggie);
   // bind drag events to Packery
   // $grid.packery( 'bindDraggabillyEvents', draggie );
 });
@@ -119,16 +119,16 @@ let cartesian = (a, b, ...c) => b ? cartesian(f(a, b), ...c) : a;
 // flatten object by concatting values, making sure to apply demorgans laws ','
 function concatValues(obj) {
 
-    value = "";
-    for (var prop in obj) {
-        var parts = obj[prop].split(",");
-        var vals = value.split(",");
-        var newvals = cartesian(parts, vals);
+  value = "";
+  for (var prop in obj) {
+    var parts = obj[prop].split(",");
+    var vals = value.split(",");
+    var newvals = cartesian(parts, vals);
 
-        value = newvals.map(x => concatArr(x)).join(',');
-    }
+    value = newvals.map(x => concatArr(x)).join(',');
+  }
 
-    return value;
+  return value;
 }
 
 // flatten object by concatting values
